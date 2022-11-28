0d61afcd0df78c8150695bfcb87358692e72702b
Wouter <wouter.meus@hotmail.com>
2022-11-28 15:39:28 +0100

    product variation

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,14 @@
-
-from django.urls import path
 from . import views
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('',views.store, name='store'),
-    path('<slug:category_slug>/', views.store, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('',views.store, name='store'),  
+    path('category/<slug:category_slug>/', views.store, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('cart/', include('carts.urls')),
+    path('search/', views.search, name='search'),
+
+
 
 ]
